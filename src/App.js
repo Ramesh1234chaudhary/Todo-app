40c7d84a9db14565b5637c8989a9a1425ac03571
ramesh <romanchaudhary01994@gmail.com>
2023-04-28 11:49:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+import './App.css';
+import OutputBox from './Components/OutputBox';
+import InputBox from './Components/InputBox';
+
+function App() {
+  const[currList,setCurrList]=useState([]);
+const [currValue,setCurrValue]=useState('');
+  return (
+    <div className="App">
+      <h1>TODO APP</h1>
+      <InputBox currList={currList} setCurrList={setCurrList} currValue={currValue} setCurrValue={setCurrValue}/>
+      <OutputBox currList={currList} setCurrList={setCurrList} currValue={currValue} setCurrValue={setCurrValue}/>
+
+    </div>
+  );
+}
+export default App;
